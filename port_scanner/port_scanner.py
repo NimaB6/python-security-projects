02eccb454cf265cd4dd9d8fe676434658d840513
Nima Bahrami <bahraminima16@gmail.com>
2026-03-22 10:12:50 +0100

    Add port scanner project

---
+++ port_scanner/port_scanner.py
@@ -0,0 +1,24 @@
+import socket
+target= input ("Enter IP address : ")
+print("Scanning started...")
+ports = {
+    21: "FTP",
+    22: "SSH",
+    80: "HTTP",
+    443: "HTTPS"
+}
+found_open = False
+for port in ports:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
+    try:
+        result = s.connect_ex((target, port))
+    
+        if result == 0:
+            print(f"Port {port} ({ports[port]}) is open")
+
+    except:
+        print("Error connecting...")
+if not found_open:
+    print("No open ports found")
+print("Scan finished")
